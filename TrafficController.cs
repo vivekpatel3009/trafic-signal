194ac6b1c88d62204ba382d68bb6528fc41265e0
vivekpatel3009 <vivekpatel3009@gmail.com>
2019-06-11 12:07:33 +0530

    save my changes

---
+++ TrafficController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace trafficsignal.Controllers
+{
+    public class TrafficController : Controller
+    {
+        // GET: Traffic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
